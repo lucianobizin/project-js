8fb9f8da8124e55245d57ae7e027eb8b7ec66502
lucianobizin <121052617+lucianobizin@users.noreply.github.com>
2023-04-15 13:47:49 +0200

    The folders and files were adapted to the new version needed

--- js/models/talleres.js
+++
@@ -1,9 +0,0 @@
-class Talleres {
-    constructor(id, nombre, precio, duracion, imagen) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.duracion = duracion;
-        this.imagen = imagen;
-    }
-}
